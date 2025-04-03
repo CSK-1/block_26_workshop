33f73130cbb916072214c21b72cfa9c528beab5d
Chase K <chasekmbll@gmail.com>
2025-04-03 14:12:18 -0500

    wrapping up - got selected contact page working

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react';
 
 function SelectedContact({selectedContactId, setSelectedContactId}){
-    const [contact, setContact] = useState(null)
+    const [contact, setContact] = useState({})
+    console.log(selectedContactId)
     useEffect(()=>{
             async function fetchContacts() {
                 try {
                     const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`);
                       const result = await response.json();
-                      setContact(result);
                       console.log(result)
+                      setContact(result);
                 } catch (error) {
                   console.error(error);
                 }
